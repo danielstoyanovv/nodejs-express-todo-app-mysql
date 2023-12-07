fb8d36d694e246f93903b52782a1dce718f58781
danielstoyanovv <daniel.stoyanovv@gmail.com>
2023-12-07 16:26:56 +0200

    Remove todo.ejs Update todo-list.js - rename todo Update todosController.ts

--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.remove-button').on('click', function(){
         var id = $(this).data('id');
-        var url = '/todo/' + id;
+        var url = '/todos/' + id;
         $.ajax({
             type: 'DELETE',
             url: url,
--- src/controllers/todosController.ts
+++ src/controllers/todosController.ts
@@ -2,13 +2,16 @@ import { Item } from '../entity/item.entity'
 import bodyParser  from 'body-parser'
 import { Request, Response } from "express"
 import { check, validationResult } from 'express-validator';
-import session from 'express-session';
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 export default function (app: any, dbDataSource: any) {
-    app.get('/todo', async function (req: Request, res: Response) {
+    app.get('/todos', async function (req: Request, res: Response) {
         const  items = await dbDataSource.getRepository(Item).find({
+            select: {
+                task: true,
+                id: true
+            },
             order: {
                 id: "DESC"
             }
@@ -17,17 +20,17 @@ export default function (app: any, dbDataSource: any) {
             const errors = req.session.errors;
             req.session.errors = [];
 
-            return res.render('todo', {
+            return res.render('todos/index', {
                 todos: items,
                 errors: errors
             });
         }
-        return res.render('todo', {
+        return res.render('todos/index', {
             todos: items
         });
     });
 
-    app.post('/todo', [urlencodedParser,
+    app.post('/todos', [urlencodedParser,
         check('task', 'Task length is between 5 and 20 characters')
             .isLength({ min: 5,  max: 20 }),
         check('email', 'Email is not valid').isEmail(),
@@ -38,19 +41,70 @@ export default function (app: any, dbDataSource: any) {
             let errors = validationResult(req)
             if (!errors.isEmpty()) {
                 req.session.errors = errors.array()
-                return res.redirect('/todo')
+                return res.redirect('/todos')
             }
             const itemObject = await dbDataSource.getRepository(Item).create(req.body)
             const item = await dbDataSource.getRepository(Item).save(itemObject)
-            return res.redirect('/todo')
+            return res.redirect('/todos')
         } catch (err) {
             console.log(err)
             req.session.errors = [{msg: "Somehting happened, or email is duplicated"}]
-            return res.redirect('/todo')
+            return res.redirect('/todos')
         }
     });
 
-    app.delete("/todo/:id", async function (req: Request, res: Response) {
+    app.get("/todos/:id", async function (req: Request, res: Response) {
+        dbDataSource.getRepository(Item).findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (result: object) {
+            if (req.session.errors) {
+                const errors = req.session.errors;
+                req.session.errors = [];
+
+                return res.render('todos/update', {
+                    errors: errors,
+                    item: result
+                });
+            }
+            return res.render('todos/update', {
+                item: result
+            });
+        }).catch(function (err: object) {
+            console.log(err)
+        })
+    })
+
+    app.post('/todos/:id', [urlencodedParser,
+        check('task', 'Task length is between 5 and 20 characters')
+            .isLength({ min: 5,  max: 20 }),
+        check('email', 'Email is not valid').isEmail(),
+        check('phone', 'Phone length is between 10 and 20 digits').isLength({ min: 10,  max: 20 })
+            .isNumeric().withMessage('Phone should be only digits')
+    ],  async function (req: Request, res: Response) {
+        try {
+            const id = req.params.id
+            let errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                req.session.errors = errors.array()
+                return res.redirect('/todos/' + id)
+            }
+            await dbDataSource
+                .createQueryBuilder()
+                .update(Item)
+                .set(req.body)
+                .where("id = :id", { id: id })
+                .execute()
+            return res.redirect('/todos/' + id)
+        } catch (err) {
+            console.log(err)
+            req.session.errors = [{msg: "Somehting happened, or email is duplicated"}]
+            return res.redirect('/todos/' + req.params.id)
+        }
+    });
+
+    app.delete("/todos/:id", async function (req: Request, res: Response) {
         try {
             const results = await dbDataSource.getRepository(Item).delete(req.params.id)
             return res.json(results)
